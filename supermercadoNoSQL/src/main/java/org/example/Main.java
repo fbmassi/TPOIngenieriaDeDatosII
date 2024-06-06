8edfacdb91c6a5f2e27babfc70e02b623f93ba93
Franco Baltasar Massi <90656227+fbmassi@users.noreply.github.com>
2024-06-06 18:18:43 -0300

    ,

--- supermercadoNoSQL/src/main/java/org/example/Main.java
+++ supermercadoNoSQL/src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import sevicios.PingService;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        PingService pingService = new PingService();
+        pingService.PingMongoDB();
+        pingService.PingRedis();
     }
 }
